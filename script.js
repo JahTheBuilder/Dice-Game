d02bea58b3e1ced2e97cb2aaf69447453b0e2761
JahTheBuilder <36336@ocvts.org>
2025-01-16 14:09:06 +0000

    not done yet 9:08 1/16/25

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 let donut = document.querySelector('.donut-cost')
 
+let clickerCost = document.querySelector('.clicker-cost')
+
+let gps = 0
+
 function incrementDonut() {
     donut.innerHTML = parseFloat(donut.innerHTML) + 1
 }
+
+function buyClicker() {
+    if (donut.innerHTML >= clickerCost.innerHTML) {
+        donut.innerHTML -= clickerCost.innerHTML
+        gps += 1
+    }
+}
+
+setInterval(() => {
+    donut.innerHTML = parseFloat(donut.innerHTML) + gps 
+}, 1000)
