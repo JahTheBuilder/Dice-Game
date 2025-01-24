333eb428cca3288c1bbc1c37a5b4047695d1832f
JahTheBuilder <36336@ocvts.org>
2025-01-24 14:10:22 +0000

    9:10 1/24/25 finished

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-let donut = document.querySelector('.donut-cost')
+let donut = document.querySelector(".donut-cost")
 let parsedDonut = parseFloat(donut.innerHTML)
 
-let clickerCost = document.querySelector('.clicker-cost')
+let clickerCost = document.querySelector(".clicker-cost")
 let parsedClickerCost = parseFloat(clickerCost.innerHTML)
 let clickerLevel = document.querySelector(".clicker-level")
 let clickerIncrease = document.querySelector(".clicker-increase")
 let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
 
-let pickaxeCost = document.querySelector('.pickaxe-cost')
+let pickaxeCost = document.querySelector(".pickaxe-cost")
 let parsedPickaxeCost = parseFloat(pickaxeCost.innerHTML)
 let pickaxeLevel = document.querySelector(".pickaxe-level")
 let pickaxeIncrease = document.querySelector(".pickaxe-increase")
 let parsedPickaxeIncrease = parseFloat(pickaxeIncrease.innerHTML)
 
-let minerCost = document.querySelector('.miner-cost')
-let parsedMinerCost = document.querySelector(minerCost.innerHTML)
+let minerCost = document.querySelector(".miner-cost")
+let parsedMinerCost = parseFloat(minerCost.innerHTML)
 let minerLevel = document.querySelector(".miner-level")
 let minerIncrease = document.querySelector(".miner-increase")
 let parsedMinerIncrease = parseFloat(minerIncrease.innerHTML)
+
+let gpcText = document.getElementById("gpc-text")
+let gpsText = document.getElementById("gps-text")
 
 let gpc = 1;
 
@@ -38,8 +41,8 @@ function buyClicker() {
         clickerIncrease.innerHTML = parsedClickerIncrease
         gpc += parsedClickerIncrease
 
-        parsedClickerCost *- 1.18;
-        clickerCost.innerHTML = Math.random(parsedClickerCost)
+        parsedClickerCost *= 1.18;
+        clickerCost.innerHTML = Math.round(parsedClickerCost)
     }
 }
 
@@ -53,12 +56,29 @@ function buyPickaxe() {
         pickaxeIncrease.innerHTML = parsedPickaxeIncrease
         gps += parsedPickaxeIncrease
 
-        parsedPickaxeCost *- 1.18;
-        pickaxeCost.innerHTML = Math.random(parsedPickaxeCost)
+        parsedPickaxeCost *= 1.18;
+        pickaxeCost.innerHTML = Math.round(parsedPickaxeCost)
+    }
+}
+
+function buyMiner() {
+    if (parsedDonut >= parsedMinerCost) {
+        donut.innerHTML = Math.round(parsedDonut -= parsedMinerCost);
+
+        minerLevel.innerHTML ++
+
+        parsedMinerIncrease = parseFloat((parsedMinerIncrease * 1.03).toFixed(2))
+        minerIncrease.innerHTML = parsedMinerIncrease
+        gpc += parsedMinerIncrease
+
+        parsedMinerCost *= 1.18;
+        minerCost.innerHTML = Math.round(parsedMinerCost)
     }
 }
 
 setInterval(() => {
     parsedDonut += gps / 10
     donut.innerHTML = Math.round(parsedDonut)
+    gpcText.innerHTML = Math.round(gpc)
+    gpsText.innerHTML = Math.round(gps);
 }, 100)
