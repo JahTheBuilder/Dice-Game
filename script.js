63a650d8e5e480027f330e4a8c67289a0e52d5ab
JahTheBuilder <36336@ocvts.org>
2025-01-27 14:57:37 +0000

    lll

--- script.js
+++ script.js
@@ -2,3 +2,34 @@ let burger = document.querySelector(".burger")
 
 let parsedBurger = parseFloat("parsedBurger")
 
+let clickerCost = document.querySelector('.clicker-cost')
+let parsedClickerCost = parseFloat(clickerCost.innerHTML)
+let clickerLevel = document.querySelector(".clicker-level")
+let clickerIncrease = document.querySelector(".clicker-increase")
+let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
+
+let gpc = 1;
+
+let gps = 0;
+
+function incrementBurger() {
+    parsedBurger += gpc;
+    burger.innerHTML = Math.round(parsedBurger += gpc);
+}
+
+function buyClicker() {
+    if (parsedBurger >= parsedClickerCost) {
+        burger.innerHTML = Math.round(parsedBurger -= parsedClickerCost);
+
+        clickerLevel.innerHTML ++
+
+        parsedClickerIncrease = parseFloat((parsedClickerIncrease *
+        1.03).toFixed(2))
+
+        clickerIncrease.innerHTML = parsedClickerIncrease
+        gpc += parsedClickerIncrease
+        
+        parsedClickerCost += 1.18;
+        clickerCost.innerHTML = Math.random(parsedClickerCost)
+    }
+}
