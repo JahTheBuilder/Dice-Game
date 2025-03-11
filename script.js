93b3127aea03fd4d1c57fb997cad6ed947cd5466
JahTheBuilder <36336@ocvts.org>
2025-03-11 12:59:54 +0000

    eh

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
-let burger = document.querySelector(".burger")
+let ice = document.querySelector(".ice-cost")
+let parsedIce = parseFloat("ice.innerHTML")
 
-let parsedBurger = parseFloat("parsedBurger")
-
-let clickerCost = document.querySelector('.clicker-cost')
+let clickerCost = document.querySelector(".clicker-cost")
 let parsedClickerCost = parseFloat(clickerCost.innerHTML)
 let clickerLevel = document.querySelector(".clicker-level")
 let clickerIncrease = document.querySelector(".clicker-increase")
 let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
 
-let pickaxeCost = document.querySelector('.pickaxe-cost')
+let pickaxeCost = document.querySelector(".pickaxe-cost")
 let parsedPickaxeCost = parseFloat(pickaxeCost.innerHTML)
 let pickaxeLevel = document.querySelector(".pickaxe-level")
 let pickaxeIncrease = document.querySelector(".pickaxe-increase")
 let parsedPickaxeIncrease = parseFloat(pickaxeIncrease.innerHTML)
 
-
-let minerCost = document.querySelector('.miner-cost')
-let parsedMinerCost = document.querySelector(minerCost.innerHTML)
-let minerLevel = document.querySelector(".miner-level")
-let minerIncrease = document.querySelector(".miner-increase")
-let parsedMinerIncrease = parseFloat(minerIncrease.innerHTML)
-
 let gpc = 1;
 
 let gps = 0;
 
-function incrementBurger() {
-    parsedBurger += gpc;
-    burger.innerHTML = Math.round(parsedBurger += gpc);
+function incrementIce() {
+    ice.innerHTML = Math.round(parsedIce += gpc);
 }
 
 function buyClicker() {
-    if  (parsedBurger >= parsedClickerCost) {
-        burger.innerHTML = Math.round(parsedBurger -= parsedClickerCost);
+    if  (parsedIce >= parsedClickerCost) {
+        ice.innerHTML = Math.round(parsedIce -= parsedClickerCost);
 
         clickerLevel.innerHTML ++
 
@@ -45,40 +36,17 @@ function buyClicker() {
     }
 }
 
-
 function buyPickaxe() {
-    if (parsedBurger >= parsedPickaxeCost) {
-        burger.innerHTML = Math.round(parsedBurger -= parsedPickaxeCost);
+    if  (parsedIce >= parsedPickaxeCost) {
+        ice.innerHTML = Math.round(parsedIce -= parsedPickaxeCost);
 
         pickaxeLevel.innerHTML ++
 
         parsedPickaxeIncrease = parseFloat((parsedPickaxeIncrease * 1.03).toFixed(2))
         pickaxeIncrease.innerHTML = parsedPickaxeIncrease
-        gps += parsedPickaxeIncrease
+        gpc += parsedPickaxeIncrease
 
         parsedPickaxeCost *= 1.18;
         pickaxeCost.innerHTML = Math.round(parsedPickaxeCost)
     }
 }
-
-function buyMiner() {
-    if (parsedBurger >= parsedMinerCost) {
-        burger.innerHTML = Math.round(parsedBurger -= parsedMinerCost);
-
-        minerLevel.innerHTML ++
-
-        parsedMinerIncrease = parseFloat((parsedMinerIncrease * 1.03).toFixed(2))
-        minerIncrease.innerHTML = parsedMinerIncrease
-        gpc += parsedMinerIncrease
-
-        parsedMinerCost *= 1.18;
-        minerCost.innerHTML = Math.round(parsedMinerCost)
-    }
-}
-
-setInterval (() => {
-    parsedBurger += gps / 10
-    donut.innerHTML = Math.round(parsedBurger)
-    gpcText.innerHTML = Math.round(gpc)
-    gpsText.innerHTML = Math.round(gps);
-}, 100)
